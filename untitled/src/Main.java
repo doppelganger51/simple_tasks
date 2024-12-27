9e50927e8659e63f970e3a5860aec7969d391839
Oleksandr Nikulshyn <65387423+doppelganger51@users.noreply.github.com>
2024-12-27 22:01:05 +0100

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 /*
-
         int a = 10;
         int b = 20;
         int t;
@@ -138,12 +137,7 @@ public class Main {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-
-
     }
 */
-
-
-
     }
 }
